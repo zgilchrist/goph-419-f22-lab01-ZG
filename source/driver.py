a9bfeb30cfaf02a91d53db9102c3415379991aeb
Zachary Gilchrist <zachary.gilchrist@ucalgary.ca>
2022-09-14 17:55:31 -0600

    added driver script for main body of code

---
+++ source/driver.py
@@ -0,0 +1,10 @@
+"""Driver script for Assignment #1
+F22 GOPH 419
+Zachary Gilchrist 10123740"""
+
+def main():
+    """Main function"""
+    print("Hello World!, This is the start of assignment #1")
+
+if __name__ == "__main__":
+    main()
