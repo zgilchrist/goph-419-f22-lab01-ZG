47579d7cfa419abc5471728cf8c75900a49a2001
zgilchrist <72540268+zgilchrist@users.noreply.github.com>
2022-10-07 22:16:12 -0600

    cleaned up files, added comments

--- source/driver.py
+++ source/driver.py
@@ -1,12 +1,23 @@
 """Driver script for Assignment #1"""
-#F22 GOPH 419 Brandon Karchewski
-#Zachary Gilchrist 10123740
+
 import matplotlib.pyplot as plt
+import numpy as np
 
 import rocket
 
 def main():
     """Main function"""
+    ###########
+    #Question 2
+    ###########
+
+    print('Computed value of min and max launch angles respectively:')
+    print(rocket.launch_angle_range(2.0,0.25,0.02))
+    print('\nTrue value of min and max laucnh angles computed using numpy:')
+
+    ###########
+    #Question 3
+    ###########
     
     #initialize arrays for first figure
     min_angles_q3 = []
@@ -20,7 +31,21 @@ def main():
         min_angles_q3.append(rocket.launch_angle_range(2.0,i,0.04)[0])
         max_angles_q3.append(rocket.launch_angle_range(2.0,i,0.04)[1])
         i+=0.001
-
+    
+    #plot of alpha_range vs angle
+    plt.figure()
+    plt.plot(alpha_range,min_angles_q3,'r',label = 'Minimum')
+    plt.plot(alpha_range,max_angles_q3, label = 'Maximum')
+    plt.xlabel('alpha')
+    plt.ylabel('angle (degrees)')
+    plt.title('Alpha Value vs. Launch Angle for ve_v0 = 2.0 and tol_alpha = 0.04')
+    plt.legend()
+    plt.savefig('alpha_range.png')
+    
+    ############
+    #Question #4
+    ############
+    
     #initialize arrays for second figure
     min_angles_q4 = []
     max_angles_q4= []
@@ -35,26 +60,7 @@ def main():
         max_angles_q4.append(rocket.launch_angle_range(j,0.25,0.04)[1])
         j+=0.01 
 
-
-    #print(alpha_range)
-    #print(min_angles_q3)
-    #print(max_angles_q3)
-    #print(min_angles_q4)
-    #print(max_angles_q4)
-
-    #plot of alpha_range vs angle
-
-    plt.figure()
-    plt.plot(alpha_range,min_angles_q3,'r',label = 'Minimum')
-    plt.plot(alpha_range,max_angles_q3, label = 'Maximum')
-    plt.xlabel('alpha')
-    plt.ylabel('angle (degrees)')
-    plt.title('Alpha Value vs. Launch Angle for ve_v0 = 2.0 and tol_alpha = 0.04')
-    plt.legend()
-    plt.savefig('alpha_range.png')
-
     #plot of velocity ratio range vs angle
-
     plt.figure()
     plt.plot(ve_v0_range,min_angles_q4, 'r',label = 'Minimum')
     plt.plot(ve_v0_range,max_angles_q4, label = 'Maximum')
--- source/rocket.py
+++ source/rocket.py
@@ -1,7 +1,4 @@
-from unittest import result
-import math
-
-import numpy
+import numpy as np
 
 def launch_angle_range(ve_v0, alpha, tol_alpha):
     """Description of function.
@@ -25,22 +22,25 @@ def launch_angle_range(ve_v0, alpha, tol_alpha):
     alpha_min = alpha*(1 - tol_alpha)
     alpha_max = alpha*(1 + tol_alpha)
 
+    #calculate values under the RHS root
     under_root_min = (1-(alpha_min/(1+alpha_min))*(ve_v0**2)) 
     under_root_max = (1-(alpha_max/(1+alpha_max))*(ve_v0**2))
 
+    #Check for negative values under root and exit program if found
     assert under_root_max > 0 and under_root_min > 0, "Complex value found, ensure values under the root are positive"
 
+    #Compute value on RHS of equation in full using numpy.sqrt
+    right_side_min = (1+alpha_min)*np.sqrt(under_root_min)
+    right_side_max = (1+alpha_max)*np.sqrt(under_root_max)
     
-    #if under_root_max < 0 or under_root_min < 0:
-    #    print(under_root_max)
-     #   print(under_root_min)
-      #  return "Error, complex value detected, ensure that value under root is positive"
-
-    right_side_min = (1+alpha_min)*numpy.sqrt(under_root_min)
-    right_side_max = (1+alpha_max)*numpy.sqrt(under_root_max)
+    #Enforce parameters of launch constraints and exit gracefully otherwise
+    assert right_side_min <= 1 and right_side_min > 0, 'Values passed to arcsin function must be less than 1 and greater than 0 for acceptable launch constraints' 
+    assert right_side_max <= 1 and right_side_max > 0, 'Values passed to arcsin function must be less than 1 and greater than 0 for acceptable launch constraints' 
     
-    phi_range = [arcsin(right_side_min),arcsin(right_side_max)]
+    #Calls arcsin function to calculate angle of launch and uses min and max alpha to place min and max angle into a numpy array of length 2
+    phi_range = np.array([arcsin(right_side_min),arcsin(right_side_max)])
     
+    #Return array to caller
     return phi_range
 
 def arcsin(x):
@@ -54,32 +54,36 @@ def arcsin(x):
     float
         The resultant value of the arcsin function.
     """
-    #Check that i is greater than or equal to one
+    #Initialize loop variables
     result = 0
     sign = False
     eps_a = 1.0
-    eps_s = 1.e-16
+    eps_s = 1.e-5
     n = 1
     fact_n = 1
     
+    #converts x to positive for purposes of the loop algorithm
     if x < 0:
         sign = True
+        x =- x
 
+    #loop that ends once the stopping criterion of five digits is met
     while eps_a > eps_s:
         numerator = (2*x)**(2*n)
         denominator = n**2*(factorial_2n(n))/(fact_n**2)
         term = numerator/denominator
         result += term
-        n+=1
+        n += 1
         fact_n *= n
-        eps_a=abs(term/result)
+        eps_a = abs(term/result)
     result = 0.5*result
-    result = numpy.sqrt(result)
+    result = np.sqrt(result)
 
+    #converts x back to negative if it was negative in the first place
     if sign == True:
         result = -result
 
-
+    #return result to caller
     return result
 
 def factorial_2n(n):
@@ -93,9 +97,9 @@ def factorial_2n(n):
     int
         The resultant value of (2n)!
     """
-    n*=2 #initialize n to be equal to 2n
+    n *= 2 #initialize n to be equal to 2n
     result = n #make initial value equal to n
-    while n>1: #multiply initial n by (n-1) terms repeatably until n reaches 1 to get our result
-        result*=(n-1)
-        n-=1
+    while n > 1: #multiply initial n by (n-1) terms repeatably until n reaches 1 to get our result
+        result *= (n-1)
+        n -= 1
     return result
